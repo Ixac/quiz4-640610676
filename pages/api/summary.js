004c7b2a729b59ecb13cb4a886a62e6a0b33e375
Ixac <ixacarrrows@gmail.com>
2022-10-06 16:00:04 +0700

    gg wp

--- pages/api/summary.js
+++ pages/api/summary.js
@@ -1,9 +1,14 @@
+import { checkToken } from "../../backendLibs/checkToken";
+import { readUsersDB } from "../../backendLibs/dbLib";
+
 export default function summaryRoute(req, res) {
   if (req.method === "GET") {
     //check authentication
-    //return res.status(403).json({ ok: false, message: "Permission denied" });
-    //compute DB summary
-    //return response
+    const user = checkToken(req);
+    if (!user || !user.isAdmin) {
+      return res.status(403).json({ ok: false, message: "Permission denied" });
+    }
+    //compute DB summary ......................asdawwthtjuyju
   } else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
   }
