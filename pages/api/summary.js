1535c4c74670c393203a81dea8f5874e0c033813
Ixac <ixacarrrows@gmail.com>
2022-10-06 16:01:06 +0700

    rekt

--- pages/api/summary.js
+++ pages/api/summary.js
@@ -8,7 +8,20 @@ export default function summaryRoute(req, res) {
     if (!user || !user.isAdmin) {
       return res.status(403).json({ ok: false, message: "Permission denied" });
     }
-    //compute DB summary ......................asdawwthtjuyju
+    //compute DB summary
+    const users = readUsersDB();
+    const admin = users.filter((x) => x.isAdmin === true);
+    const usernum = users.filter((x) => x.isAdmin === false);
+    const sum = usernum.reduce((xd, dx) => {
+      return (xd += dx.money);
+    }, 0);
+
+    return res.json({
+      ok: true,
+      userCount: usernum.length,
+      adminCount: admin.length,
+      totalMoney: sum,
+    });
   } else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
   }
